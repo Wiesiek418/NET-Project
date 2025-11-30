691fe8dbf498876cb5719d980851016e6f84241f
Pawel Bogdanowicz <pawelb021@gmail.com>
2025-11-30 22:00:13 +0100

    it work now

--- api/api_src/Domains/Blockchain/Infrastructure/BlockchainSettings.cs
+++ api/api_src/Domains/Blockchain/Infrastructure/BlockchainSettings.cs
@@ -10,6 +10,8 @@ public class BlockchainSettings
     public string RpcUrl { get; set; } = "https://sepolia.infura.io/v3/3a9054388e6748308aac294ff334b4c2";
     public string TokenAddress { get; set; } = "0x76a7854fB1f3bAd944F7aD88D5b9f749430f5613";
     public int TokenDecimals { get; set; } = 18;
+    public string AppWalletPrivateKey { get; set; } = string.Empty;
+    public decimal PaymentAmount { get; set; } = 0.001m;
     public BlockchainCollectionsSettings Wallets { get; set; } = new();
 }
 
